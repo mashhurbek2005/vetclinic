e8b64d0cb4bf6064623f865c4fcfaca17fd7a980
mashhurbek2005 <muhammedovmashhurbek@gmail.com>
2022-07-20 16:09:43 +0500

    Add services section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var elModalOpenButton = document.querySelector(".js-modal-open-button");
+var elModal = document.querySelector(".modal");
+
+elModalOpenButton.addEventListener("click", function(){
+    elModal.classList.add("modal--show");
+})
+
+var elModalCloseButton = document.querySelector(".js-close-modal");
+
+elModalCloseButton.addEventListener("click", function(){
+    elModal.classList.remove("modal--show");
+})
